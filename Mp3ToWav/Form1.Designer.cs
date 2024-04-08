971ff3b09c63f340393928cfaf3e69c73cea6109
unknown <ethemdemirkaya189@gmail.com>
2024-04-08 14:58:29 +0300

    Created codzen converter.

---
+++ Mp3ToWav/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Mp3ToWav
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.svgImageBox1 = new DevExpress.XtraEditors.SvgImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Question;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Location = new System.Drawing.Point(1006, 91);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(169, 73);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Dosya Seçiniz";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // svgImageBox1
+            // 
+            this.svgImageBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.svgImageBox1.Location = new System.Drawing.Point(10, 13);
+            this.svgImageBox1.Name = "svgImageBox1";
+            this.svgImageBox1.Size = new System.Drawing.Size(332, 200);
+            this.svgImageBox1.SizeMode = DevExpress.XtraEditors.SvgImageSizeMode.Zoom;
+            this.svgImageBox1.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageBox1.SvgImage")));
+            this.svgImageBox1.TabIndex = 1;
+            this.svgImageBox1.Text = "svgImageBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(348, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CodZen Mp3 to Wav Converter";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(348, 170);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(827, 43);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(344, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 46);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dönüştürülüyor...";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(1198, 239);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.svgImageBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CodZen Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SvgImageBox svgImageBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Mp3ToWav/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace Mp3ToWav
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ConvertMp3ToWavWithProgressBar(string mp3FilePath, string wavFilePath)
+        {
+            progressBar1.Visible = true;
+            label2.Visible = true;
+            using (Mp3FileReader reader = new Mp3FileReader(mp3FilePath))
+            {
+                // Örnekleme frekansını 44100 Hz olarak ayarlayın ve 16 bit derinlik kullanın
+                var waveFormat = new WaveFormat(44100, 16, 1); // Mono (tek kanallı) ses
+
+                // PCM akışını oluşturun ve önceki WaveFormat'ten yeni WaveFormat'e dönüştürün
+                using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+                {
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = (int)reader.Length;
+
+                    // Normalleştirme işlemi için ölçek faktörü hesaplayın
+                    float max = 0;
+                    byte[] tempBuffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = pcmStream.Read(tempBuffer, 0, tempBuffer.Length)) > 0)
+                    {
+                        for (int i = 0; i < bytesRead / 2; i += 2) // 16 bit için ikişer adımla ilerle
+                        {
+                            short sample = BitConverter.ToInt16(tempBuffer, i);
+                            float sampleAbs = Math.Abs(sample);
+                            if (sampleAbs > max)
+                                max = sampleAbs;
+                        }
+                    }
+
+                    float scale = short.MaxValue / max;
+
+                    // PCM akışını tekrar okuyarak normalleştirme işlemi uygulayın ve WAV dosyasına yazın
+                    pcmStream.Position = 0; // Akışı başa al
+                    using (WaveFileWriter writer = new WaveFileWriter(wavFilePath, waveFormat))
+                    {
+                        while ((bytesRead = pcmStream.Read(tempBuffer, 0, tempBuffer.Length)) > 0)
+                        {
+                            for (int i = 0; i < bytesRead; i += 2)
+                            {
+                                short sample = BitConverter.ToInt16(tempBuffer, i);
+                                float normalizedSample = sample * scale;
+                                byte[] normalizedSampleBytes = BitConverter.GetBytes((short)normalizedSample);
+                                writer.Write(normalizedSampleBytes, 0, normalizedSampleBytes.Length);
+                            }
+                            progressBar1.Value = (int)reader.Position;
+                        }
+                    }
+                }
+            }
+        }
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string mp3FilePath = openFileDialog1.FileName;
+                string wavFilePath = mp3FilePath.Replace(".mp3", ".wav");
+
+                ConvertMp3ToWavWithProgressBar(mp3FilePath, wavFilePath);
+
+                MessageBox.Show("Dönüşüm tamamlandı!");
+                progressBar1.Visible = false;
+                label2.Visible = false;
+            }
+        }
+    }
+}
+
